7a9aa463f6ddba4a51d52c79ae08b6f253d5dd29
Mario dell'Oglio <35237827+Marioemtec@users.noreply.github.com>
2026-03-29 16:00:59 +0200

    fix: health

--- src/main/java/com/clinicaplus/controller/HealthCheckController.java
+++ src/main/java/com/clinicaplus/controller/HealthCheckController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthCheckController {
 
-    @GetMapping("/api/health")
+    @GetMapping("/health")
     public String healthCheck() {
         return "Backend is working!";
     }
